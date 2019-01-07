7cb5460a12e637c1e594ac5841c1e5c089abc40c
Edvaldo Segundo <edvaldo.segundo@hotmail.com>
2019-01-07 21:12:12 +0000

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department department = new Department(2, "Books");
+		
+		System.out.println(department);
+
+	}
+
+}
